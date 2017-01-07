a6c14eeef45793b861bc854b9810c913bd3080f1
Yanshuo H <huangyanshuo@meican.com>
2017-01-07 10:12:29 +0800

    resp helper

---
+++ utils/response.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"github.com/YanshuoH/youkonger/consts"
+)
+
+type JSONResponse struct {
+	ResultCode        string      `json:"resultCode"`
+	ResultDescription string      `json:"resultDescription"`
+	Data              interface{} `json:"data"`
+}
+
+func NewJSONResponse(resultCode string, datas ...interface{}) JSONResponse {
+	r := JSONResponse{
+		ResultCode:        resultCode,
+		ResultDescription: consts.Messenger.Get(resultCode),
+	}
+	if len(datas) > 0 {
+		r.Data = datas[0]
+	}
+
+	return r
+}
---
+++ utils/response_test.go
@@ -0,0 +1,37 @@
+package utils_test
+
+import (
+	. "github.com/YanshuoH/youkonger/utils"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/YanshuoH/youkonger/consts"
+)
+
+var _ = Describe("Response", func() {
+	Describe("NewJSONResponse", func() {
+		Context("With optional argument as data", func() {
+			It("Should return the argument value as data field", func() {
+				By("string type as data")
+				r := NewJSONResponse(consts.EventNotFound, "something wrong")
+				Expect(r.ResultCode).To(Equal(consts.EventNotFound))
+				Expect(r.ResultDescription).To(Equal(consts.Messenger.Get(consts.EventNotFound)))
+				Expect(r.Data).To(Equal("something wrong"))
+
+				By("array type as data")
+				d1 := []string{"a", "b", "c"}
+				r = NewJSONResponse(consts.OK, d1)
+				Expect(r.Data).To(Equal(d1))
+
+				By("struct type as data")
+				d2 := struct {
+					T int
+				}{
+					T: 123,
+				}
+				r = NewJSONResponse(consts.OK, d2)
+				Expect(r.Data).To(Equal(d2))
+			})
+		})
+	})
+})
