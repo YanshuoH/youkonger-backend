3cca7ecac259551445183685670960331c92caf0
Yanshuo H <huangyanshuo@meican.com>
2017-01-15 13:30:56 +0800

    initial serverside data renderer

--- controllers/views.go
+++ controllers/views.go
@@ -3,10 +3,19 @@ package controllers
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"github.com/YanshuoH/youkonger/jrenders"
+	"github.com/YanshuoH/youkonger/dao"
+	"github.com/YanshuoH/youkonger/models"
+	"encoding/json"
 )
 
 func OK(c *gin.Context) {
+	e := &models.Event{}
+	dao.Event.First(e)
+
+	// @TODO: handle exception
+	jsonByte, _ := json.Marshal(jrenders.Event.Itemize(e, jrenders.EventParam{ShowHash: true}))
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"title": "Main website",
+		"Initial": string(jsonByte),
 	})
 }
